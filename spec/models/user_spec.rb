95c1f9553bdcd9054580d2e0bd387b44cc68ed8d
Jason Reeder <info@jasonreeder.com>
2017-05-04 17:36:11 -0700

    Add validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it 'should do things'
+    # Set subject in order to validate uniqueness
+    subject { User.new(password: "password") }
+
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+    it { should validate_uniqueness_of(:username) }
   end
 
   describe 'associations'
